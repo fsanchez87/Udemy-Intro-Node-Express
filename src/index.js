cc9639ddc42d89250a176e1e1c15993f8411d81e
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-01-23 16:39:42 +0100

    4. Configurando npm en nuestro proyecto

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var http = require("http");
 var server = http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Hello, world");
+    res.write("Hello, world!");
     res.end();
   })
-  .listen(5000);
+  .listen(3000);
