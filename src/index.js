d75f139f202903eb3bdf4d99466421ec52796b66
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-01-23 13:27:07 +0100

    3.Modulos del nucleo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-function sum(num1, num2) {
-  return num1 + num2;
-}
+var http = require("http");
 
-console.log("Hello world!", sum(10, 30));
+var server = http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello, world");
+    res.end();
+  })
+  .listen(5000);
