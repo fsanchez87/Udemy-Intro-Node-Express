b7f4b7f91f28e32845cbd420aa9e0deaa16d7afd
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-01-27 14:08:01 +0100

    12. Creando una app express

--- src/index.js
+++ src/index.js
@@ -1,57 +1,58 @@
-const http = require("http");
-const url = require("url");
+const express = require("express");
 const { multiplicacion } = require("./utils/operations");
 const { phone } = require("phone");
 
 const PORT = 3000;
+const app = express();
 
-const server = http
+app.get("/", (req, res) => {
+  res.status(200).send("Hello, HOME !");
+});
 
-  .createServer((req, res) => {
+app.listen(PORT, () => {
+  console.log("Running on " + PORT);
+});
 
-    console.log(req.url);
+// const server = http.createServer((req, res) => {
+//   console.log(req.url);
 
-    const urlData = url.parse(req.url, true);
-    const path = urlData.pathname;
-    const query = urlData.query
-    console.log("PATH", path);
-    console.log("QUERY", query);
+//   const urlData = url.parse(req.url, true);
+//   const path = urlData.pathname;
+//   const query = urlData.query;
+//   console.log("PATH", path);
+//   console.log("QUERY", query);
 
-    switch (path) {
-      case "/":
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write("Hello, HOME!");
-        break;
-      case "/info":
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.write(
-          JSON.stringify({ name: "udemy-intro-node-express", version: "1.0.0" })
-        );
-        break;
-      case "/detail":
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write("Hello, DETAIL!");
-        break;
+//   switch (path) {
+//     case "/":
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write("Hello, HOME!");
+//       break;
+//     case "/info":
+//       res.writeHead(200, { "Content-Type": "application/json" });
+//       res.write(
+//         JSON.stringify({ name: "udemy-intro-node-express", version: "1.0.0" })
+//       );
+//       break;
+//     case "/detail":
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write("Hello, DETAIL!");
+//       break;
 
-      case "/phone":
-        try {
-          const result = phone(query.value, {country: query.country}); 
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.write(JSON.stringify(result));
-          
-        } catch (error) {
-          res.writeHead(400, { "Content-Type": "text/html" });
-          res.write("Bad response,", error.message);
-        }
-        break;
+//     case "/phone":
+//       try {
+//         const result = phone(query.value, { country: query.country });
+//         res.writeHead(200, { "Content-Type": "application/json" });
+//         res.write(JSON.stringify(result));
+//       } catch (error) {
+//         res.writeHead(400, { "Content-Type": "text/html" });
+//         res.write("Bad response,", error.message);
+//       }
+//       break;
 
-      default:
-        res.writeHead(400, { "Content-Type": "text/html" });
-        res.write("Hello, not fount!");
-        break;
-    }
-    res.end();
-  })
-  .listen(PORT);
+//     default:
+//       res.writeHead(400, { "Content-Type": "text/html" });
+//       res.write("Hello, not fount!");
+//       break;
+//   }
+// });
 
-console.log("🚀 Multiplicación:", multiplicacion(1, 2));
