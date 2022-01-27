df8b9d0b7f7e33f3017c559a0efcc6ff29f30b20
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-01-27 23:45:08 +0100

    13. Rutas de nuestra app con express

--- src/index.js
+++ src/index.js
@@ -9,50 +9,39 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello, HOME !");
 });
 
+app.get("/info", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res
+    .status(200)
+    .send(
+      JSON.stringify({ name: "udemy-intro-node-express", version: "1.0.0" })
+    );
+});
+
+app.get("/detail", (req, res) => {
+  res.status(200).send("Hello, DETAIL!");
+});
+
+app.get("/detail", (req, res) => {
+  res.status(200).send("Hello, DETAIL!");
+});
+
+app.get("/phone", (req, res) => {
+  try {
+    const { value, country } = req.query;
+    const result = phone(value, { country: country });
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(JSON.stringify(result));
+  } catch (error) {
+    res.status(400).send("Bad response,", error.message);
+  }
+});
+
+app.use((req, res) =>{
+  res.status(404).send("NOT FOUND!");
+});
+
 app.listen(PORT, () => {
   console.log("Running on " + PORT);
 });
-
-// const server = http.createServer((req, res) => {
-//   console.log(req.url);
-
-//   const urlData = url.parse(req.url, true);
-//   const path = urlData.pathname;
-//   const query = urlData.query;
-//   console.log("PATH", path);
-//   console.log("QUERY", query);
-
-//   switch (path) {
-//     case "/":
-//       res.writeHead(200, { "Content-Type": "text/html" });
-//       res.write("Hello, HOME!");
-//       break;
-//     case "/info":
-//       res.writeHead(200, { "Content-Type": "application/json" });
-//       res.write(
-//         JSON.stringify({ name: "udemy-intro-node-express", version: "1.0.0" })
-//       );
-//       break;
-//     case "/detail":
-//       res.writeHead(200, { "Content-Type": "text/html" });
-//       res.write("Hello, DETAIL!");
-//       break;
-
-//     case "/phone":
-//       try {
-//         const result = phone(query.value, { country: query.country });
-//         res.writeHead(200, { "Content-Type": "application/json" });
-//         res.write(JSON.stringify(result));
-//       } catch (error) {
-//         res.writeHead(400, { "Content-Type": "text/html" });
-//         res.write("Bad response,", error.message);
-//       }
-//       break;
-
-//     default:
-//       res.writeHead(400, { "Content-Type": "text/html" });
-//       res.write("Hello, not fount!");
-//       break;
-//   }
-// });
 
