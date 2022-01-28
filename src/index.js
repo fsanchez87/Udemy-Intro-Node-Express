50f6f06ce316df05999c2536ae96b9b1eba539f4
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-01-28 10:37:58 +0100

    15. Ruta de tipo POST

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { multiplicacion } = require("./utils/operations");
+const bodyParser = require("body-parser");
 const { phone } = require("phone");
+const { restart } = require("nodemon");
 
 const PORT = 3000;
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello, HOME !");
@@ -26,6 +29,14 @@ app.get("/detail", (req, res) => {
   res.status(200).send("Hello, DETAIL!");
 });
 
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  const { username, password } = req.body;
+  username === "paco" && password === "2345"
+    ? res.send({ status: "ok" })
+    : res.status(401).send({ status: "Error" });
+});
+
 app.get("/phone", (req, res) => {
   try {
     const { value, country } = req.query;
@@ -37,11 +48,10 @@ app.get("/phone", (req, res) => {
   }
 });
 
-app.use((req, res) =>{
+app.use((req, res) => {
   res.status(404).send("NOT FOUND!");
 });
 
 app.listen(PORT, () => {
   console.log("Running on " + PORT);
 });
-
