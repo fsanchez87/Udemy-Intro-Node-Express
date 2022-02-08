3d8cd45c28c8a60be825d81ad7c787cf032d2934
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-02-08 22:42:06 +0100

    17. reqres.in - usuario por id

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,6 +2,7 @@ const userController = require("../../controllers/v1/users-controller");
 
 const createRoutesV1 = (app) => {
   app.get("/api/v1/users", userController.getUsers);
+  app.get("/api/v1/users/:userId", userController.getUserById);
 };
 
 module.exports = createRoutesV1;
