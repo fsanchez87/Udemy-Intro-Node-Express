65d48d9252a620f395949d8194cc6ac527fc14dd
Francisco S <sanchezgarridof@gmail.com>
2022-02-23 00:06:59 +0100

    Typescript

---
+++ typescript/dist/notes.js
@@ -0,0 +1,33 @@
+"use strict";
+// VARIABLES Y CONSTANTES
+let variable = "variable";
+const constante = 1234;
+// TIPOS DE DATOS MAS COMUNES
+let age = 35;
+const apiKey = "1234";
+let isActive = false;
+const numbers = [];
+const numbers2 = [];
+const names = ["John", "Jane"];
+// OBJETOS
+let user1 = {
+    name: "John",
+    lastName: "Jane",
+    age: 35,
+};
+let user2 = {
+    name: "John",
+    lastName: "Jane",
+    age: 35,
+};
+// CLASES
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    toString() {
+        return `Nombre: ${this.name}, edad: ${this.age}`;
+    }
+}
+const dog = new Animal("Nero", 10);
