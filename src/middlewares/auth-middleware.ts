6b020f591dc7f512ada92465b69be24270456ca1
Francisco S <sanchezgarridof@gmail.com>
2022-03-10 08:53:18 +0100

    79. Middlewares - parte 1

---
+++ src/middlewares/auth-middleware.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const checkAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  try {
+    const { token } = req.headers;
+
+    if (!token) {
+      throw new Error('Missing header token');
+    }
+
+    jwt.verify(token as string, process.env.JWT_SECRET!);
+    next();
+
+  } catch (e: any) {
+    res.status(401).send(e.message);
+  }
+};
--- src/routes/v1/product-router.ts
+++ src/routes/v1/product-router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
 import * as productController from '../../controllers/v1/products-controllers';
+import { checkAuth } from '../../middlewares/auth-middleware';
 
 const routes = Router();
 
-routes.get('/', productController.getProducts);
+routes.get('/',checkAuth, productController.getProducts);
 routes.get('/:productId', productController.getProductById);
 routes.post('/create', productController.createProduct);
 routes.put(
