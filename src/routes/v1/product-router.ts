c38efcbc50f90159cad0473a18a9f50d354fb552
Francisco S <sanchezgarridof@gmail.com>
2022-03-14 10:27:37 +0100

    82. Validaciones - parte 1 15 min

--- src/routes/v1/product-router.ts
+++ src/routes/v1/product-router.ts
@@ -1,13 +1,28 @@
-import { Router } from 'express';
-
+import { Router, Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
 import * as productController from '../../controllers/v1/products-controllers';
-import { checkAuth, checkIP } from '../../middlewares/auth-middleware';
+import { checkAuth } from '../../middlewares/auth-middleware';
+import { validateNewProductBody } from '../../validators/v1/products-validator';
 
 const routes = Router();
 
 routes.get('/', checkAuth, productController.getProducts);
 routes.get('/:productId', checkAuth, productController.getProductById);
-routes.post('/create', checkAuth, productController.createProduct);
+routes.post(
+  '/create',
+  checkAuth,
+  validateNewProductBody,
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      next()
+    }else{
+      res.status(404).send(errors.array());
+    }
+  },
+
+  productController.createProduct
+);
 routes.put('/products/:productId', checkAuth, productController.updateProduct);
 routes.delete(
   '/products/:productId',
---
+++ src/validators/v1/products-validator.ts
@@ -0,0 +1,12 @@
+import { checkSchema } from 'express-validator';
+
+export const validateNewProductBody = checkSchema({
+  year: {
+    isInt: true,
+    customSanitizer: {
+      options: (value) => {
+        return parseInt(value);
+      },
+    },
+  },
+});
